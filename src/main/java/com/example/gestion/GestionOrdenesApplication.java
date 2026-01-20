a7021033463d14d40246d9b90a546c0648b4d3fc
Mateoro1705 <mateorodriguez1705@yahoo.com>
2026-01-20 10:20:54 -0500

    Creacion de las carpetas

---
+++ src/main/java/com/example/gestion/GestionOrdenesApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionOrdenesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionOrdenesApplication.class, args);
+	}
+
+}
